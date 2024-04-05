4baaadd2b90d38714fdba3eea8e945b4412b450e
Denys Bieliaiev <81828135+dencx@users.noreply.github.com>
2024-04-05 23:45:04 +0200

    add saving data to the cache

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Book;
+use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
@@ -29,7 +30,10 @@ class BookController extends Controller
             default => $books->latest(),
         };
 
-        $books = $books->get();
+        // $books = $books->get();
+
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn () => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
